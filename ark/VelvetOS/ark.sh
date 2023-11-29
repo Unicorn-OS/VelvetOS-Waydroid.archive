e03f996cba16b06b55985b471d5aae9ad3603ee1
🦄Pegasus (DarkStar☀) <darkstar@unisocial.net>
2023-11-29 13:30:31 -0600

    hexdump

---
+++ ark/VelvetOS/ark.sh
@@ -0,0 +1,5 @@
+wget https://github.com/hexdump0815/linux-mainline-mediatek-mt81xx-kernel/archive/refs/heads/master.zip -O linux-mainline-mediatek-mt81xx-kernel.zip
+
+wget https://github.com/hexdump0815/kernel-config-options/archive/refs/heads/master.zip -O kernel-config-options
+
+#wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.1.tar.xz -O linux-5.19.1.tar.xz
